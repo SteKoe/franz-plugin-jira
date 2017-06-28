a30a157d6d71f9ba4202e25ccbfd22319f6e5de8
Lukas Gräßle <Lukas.Graessle@web.de>
2017-06-28 18:15:41 +0200

    feat(jira): open external links in the same window (#1)
    * feat(href): open external links in the same window
    
    * feat(history): add back button
    
    * chore(chore): code formatting

--- webview.js
+++ webview.js
@@ -1,3 +1,25 @@
 module.exports = (Franz, options) => {
+    (function (window) {
+        const openExternal = require('electron').shell.openExternal;
+        
+        window.addEventListener('click', (event) => {
+            const baseRegex = new RegExp(`${ window.location.hostname}`);
+            if (event.target.tagName === 'A' && !baseRegex.test(event.target.href)) {
+                openExternal(event.target.href);
+                event.preventDefault();
+            }
+        });
 
+        var backButton = document.createElement("a");
+        backButton.style.position = 'relative';
+        backButton.innerHTML = "Back";
+        backButton.onclick = function () {
+            window.history.back();
+        };
+
+	var li = document.createElement("li");
+        li.appendChild(backButton);
+        document.getElementsByClassName("aui-nav")[0].appendChild(li);
+
+    })(window);
 }
